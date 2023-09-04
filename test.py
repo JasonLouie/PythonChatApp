e5c4f74eaa108c829e585e318868708afa643b97
Jason Louie <jason.louie29@myhunter.cuny.edu>
2023-09-04 17:04:08 -0400

    Deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import threading
-
-def printit(some_str: str, num=0):
-    if num != 0:
-        threading.Timer(5.0, printit, [some_str, num-1]).start()
-        print(some_str)
-
-printit("Bob", 2)
